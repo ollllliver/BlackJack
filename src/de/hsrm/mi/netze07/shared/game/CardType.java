f66c45e3fb92eddc8145038ca3d524a21c96e641
Oliver Folwarzny <ofolwarzny@coconut-capital.com>
2020-05-20 19:30:55 +0200

    Add value to CardType for message identification

--- src/de/hsrm/mi/netze07/shared/game/CardType.java
+++ src/de/hsrm/mi/netze07/shared/game/CardType.java
@@ -1,8 +1,18 @@
 package de.hsrm.mi.netze07.shared.game;
 
 public enum CardType {
-    DIAMONDS,
-    HEARTS,
-    SPADES,
-    CLUBS
+    DIAMONDS(1),
+    HEARTS(2),
+    SPADES(3),
+    CLUBS(4);
+
+    private final int value;
+
+    CardType(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
 }
