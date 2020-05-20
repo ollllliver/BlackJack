465c310e19ee9b3a053192bd107e80ca1344f9ea
Marvin Lindner <60143015+marv9504@users.noreply.github.com>
2020-05-20 22:52:31 +0200

    Client listener, Main updates

--- src/de/hsrm/mi/netze07/client/Client.java
+++ src/de/hsrm/mi/netze07/client/Client.java
@@ -4,26 +4,87 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class Client {
 
-	DataOutputStream outToServer;
-	BufferedReader inFromServer;
+	private DataOutputStream outToServer;
+	private BufferedReader inFromServer;
+	private Socket clientSocket;
+	private Thread listenThread;
 
-	public void initialize(Socket clientSocket) throws IOException {
+	public Client(String HOST, int PORT) throws UnknownHostException, IOException {
+		this.clientSocket = new Socket(HOST, PORT);
+	}
+
+	public void initialize() throws IOException {
 		outToServer = new DataOutputStream(clientSocket.getOutputStream());
 		inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 	}
-	
 
 	public void outToServer(String sentence) throws IOException {
 		outToServer.writeBytes(sentence + '\n');
 	}
-	
 
 	public String inFromServer() throws IOException {
 		return inFromServer.readLine();
+	}
+
+	public void closeConnection() throws IOException {
+		clientSocket.close();
+	}
+
+	public void listen() {
+		if (listenThread != null) {
+			listenThread.interrupt();
+			listenThread = null;
+		}
+		
+		listenThread = new Thread(() -> {
+			
+			while(!Thread.interrupted()) {
+				
+				try {
+					
+					final String raw = inFromServer.readLine();
+					
+					if(Thread.interrupted()) {
+						break;
+					}
+					
+					if(raw == null) {
+						break;
+					}
+					
+					//handleMessage(raw);
+					System.out.println(raw);
+					
+				} catch (SocketException e) {
+					if(Thread.interrupted()) {
+						System.out.println("Thread endet wie gewünscht");
+					} else {
+						e.printStackTrace();
+					}
+					
+					break;
+				} catch (IOException e) {
+					e.printStackTrace();
+					break;
+				}
+			}
+			
+		});
+		
+		listenThread.start();
+
+	}
+
+	private void handleMessage(String raw) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- src/de/hsrm/mi/netze07/client/Main.java
+++ src/de/hsrm/mi/netze07/client/Main.java
@@ -1,36 +1,38 @@
 package de.hsrm.mi.netze07.client;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Main {
-	
+
 	public static final int PORT = 29000;
 	public static final String HOST = "127.0.0.1";
 	static String sentence;
 	static String modifiedSentence;
-	
 
 	public static void main(String[] args) throws UnknownHostException, IOException {
 
-		BufferedReader playerInput = new BufferedReader(new InputStreamReader((System.in)));
-		Socket clientSocket = new Socket(HOST, PORT);
-		Client client = new Client();
-		
-		client.initialize(clientSocket);
-		
+		// listen methode wie im tic tac toe, damit client die ganze zeit änderngen
+		// erwartet
+		// damit fluss steht, client bekommt nachricht, client sendet nachricht
 
+		BufferedReader playerInput = new BufferedReader(new InputStreamReader((System.in)));
+		Client client = new Client(HOST, PORT);
+
+		client.initialize();
+		client.listen();
+		
 		sentence = playerInput.readLine();
+		
 		client.outToServer(sentence);
 		
-		modifiedSentence = client.inFromServer();
-		System.out.println("Antwort vom Dealer: " + modifiedSentence);
-		
-		clientSocket.close();
-		
+		// modifiedSentence = client.inFromServer();
+		// System.out.println("Antwort vom Dealer: " + modifiedSentence);
+
+		client.closeConnection();
+
 	}
 }
