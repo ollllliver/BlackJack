96c50e92dd96022c62e2663d906bac4daec4cbac
Oliver Renth <oliver.renth@gmail.com>
2020-05-23 14:48:02 +0200

    editing of TakeCardMessageHandler

--- src/de/hsrm/mi/netze07/server/messaging/TakeCardMessageHandler.java
+++ src/de/hsrm/mi/netze07/server/messaging/TakeCardMessageHandler.java
@@ -11,13 +11,30 @@ public class TakeCardMessageHandler implements IMessageHandler{
 	@Override
 	public void handleMessage(Message message, Table table, NetworkService service) {
 		//Take card for player
-		Card card = table.addPlayerCard();
+		Card playerCard = table.addPlayerCard();
+        service.write(MessageGenerator.playerCard(playerCard));
 		if(table.getPlayerValue()>21) {
             service.write(MessageGenerator.gameEnd(GameStatus.LOOSE));
             table.setTerminated(true);
 		}
-		else {
-            service.write(MessageGenerator.playerCard(card));
+		else if(table.getPlayerValue()==21) {
+			//show hidden dealer card
+			Card dealerCard = table.addDealerCard(table.getHiddenDealerCard());
+			service.write(MessageGenerator.showDealerCard(dealerCard));
+			//take dealer cards until minimum 17
+			while(table.getDealerValue()<17) {
+				dealerCard= table.addDealerCard();
+				service.write(MessageGenerator.showDealerCard(dealerCard));
+			}
+			
+			if(table.getDealerValue()==21) {
+				service.write(MessageGenerator.gameEnd(GameStatus.DRAW));
+	            table.setTerminated(true);
+			}
+			else {
+				service.write(MessageGenerator.gameEnd(GameStatus.WIN));
+	            table.setTerminated(true);
+			}
 		}
 	}
 
